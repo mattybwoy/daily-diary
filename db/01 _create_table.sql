1158ea811b91528d7ea544be161a44e96ad3915e
Matthew Lock <matthew_lock20@hotmail.com>
2020-07-05 15:03:28 +0100

    Created db files

--- db/01 _create_table.sql
+++ db/01 _create_table.sql
@@ -7,3 +7,5 @@ CREATE TABLE entry
   entry VARCHAR(280),
   posting_date DATE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Duplicate database constructed using above commands except database is called chitter_test
